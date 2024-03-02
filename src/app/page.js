070cc72d3085914b7183e589292372383a7e1005
Matheus B <matheus_ob59@hotmail.com>
2024-03-01 21:59:19 -0300

    Add kendo grid with fake data

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,39 @@
+'use client';
+
+import products from './fakeData.json';
+import { useState } from 'react';
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import { orderBy } from '@progress/kendo-react-data-tools';
+
+const initialSort = [
+  {
+    field: 'UnitPrice',
+    dir: 'asc',
+  },
+];
+
 export default function Home() {
-  return <main>Hello</main>;
+  const [sort, setSort] = useState(initialSort);
+
+  return (
+    <main className='flex flex-col items-center justify-center h-screen'>
+      <div className='w-9/12'>
+        <Grid
+          style={{
+            height: '400px',
+          }}
+          data={orderBy(products, sort)}
+          sortable={true}
+          sort={sort}
+          onSortChange={(e) => setSort(e.sort)}
+        >
+          <GridColumn field='ProductID' title='ID' width='40px' />
+          <GridColumn field='ProductName' title='Name' width='250px' />
+          <GridColumn field='Category.CategoryName' title='CategoryName' />
+          <GridColumn field='UnitPrice' title='Price' />
+          <GridColumn field='UnitsInStock' title='In stock' />
+        </Grid>
+      </div>
+    </main>
+  );
 }
